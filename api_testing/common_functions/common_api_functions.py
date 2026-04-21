ac187ac9624d717fcfc377d087154e062679ee10
nichasius koech <nichasius.koech@dxc.com>
2026-04-21 16:11:29 +0200

    API Testing practice

--- api_testing/common_functions/common_api_functions.py
+++
@@ -1,7 +0,0 @@
-"""This module contains common functions for API Testing."""
-from api_testing.api_data.api_test_data import ApiTestData
-
-def end_point(id:int)-> str:
-    """Return resource from endpoint."""
-    return f"{ApiTestData.base_url}objects/{id}"
-
